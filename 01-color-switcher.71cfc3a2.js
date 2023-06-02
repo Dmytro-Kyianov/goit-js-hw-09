093ef55c44745bd64d0ad42a4a03fc6ac018d591
Dmytro-Kyianov <118774274+Dmytro-Kyianov@users.noreply.github.com>
2023-06-02 17:42:01 +0000

    Deploying to gh-pages from @ Dmytro-Kyianov/goit-js-hw-09@09d7fdc4e085047a7c6cc0ba0250d35179160aea 🚀

---
+++ 01-color-switcher.71cfc3a2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}));var n=null}();
+//# sourceMappingURL=01-color-switcher.71cfc3a2.js.map
